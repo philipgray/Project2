ba55ccfc6b6285832834f9eab8830529ebbe9c45
AlexWills37 <alexanderwills37@gmail.com>
2022-03-05 17:46:42 -0500

    Beginning work on saving/loading

---
+++ src/SlideDeckFile.java
@@ -0,0 +1,25 @@
+
+/**
+ * Class to manage saving and loading files. Saves and loads files as JSONs
+ * 
+ * @author Alex Wills
+ * @date 5 March 2022
+ */
+public class SlideDeckFile {
+    
+
+
+    /**
+     * Loads a .json file to create a slide deck
+     * 
+     * @param filePath the path of the file to load
+     * @return the SlideDeck specified by the file
+     */
+    public static SlideDeck loadFile(String filePath){
+        SlideDeck deck = new SlideDeck();
+
+
+        
+        return deck;
+    }
+}
