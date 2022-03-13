fbc89a7c506319af45006c2ddffe6953b6205dad
Philip Gray <philiprgray@msn.com>
2022-03-13 13:55:10 -0400

    eeek! InvisibleTextField didnt get added

---
+++ src/GUI/InvisibleTextField.java
@@ -0,0 +1,83 @@
+// https://stackoverflow.com/questions/33949537/drawing-text-from-keyboard-input-on-a-canvas?rq=1
+
+package GUI;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.text.*;
+
+public class InvisibleTextField extends JTextField
+        implements ActionListener, FocusListener, MouseListener, DocumentListener
+{
+    DrawingPanel drawingPanel;
+    public InvisibleTextField(DrawingPanel drawingPanel)
+    {
+        this.drawingPanel = drawingPanel;
+        setOpaque( false );
+        setColumns( 1 );
+//      setBorder( null );
+        setSize( getPreferredSize() );
+        setColumns( 0 );
+        addActionListener( this );
+        addFocusListener( this );
+        addMouseListener( this );
+        getDocument().addDocumentListener( this );
+    }
+
+//  Implement ActionListener
+
+    public void actionPerformed(ActionEvent e)
+    {
+        System.out.println("action Performed");
+        setEditable( false );
+        drawingPanel.removeText(this);
+    }
+
+//  Implement FocusListener
+
+    public void focusLost(FocusEvent e)
+    {
+        System.out.println("focus lost");
+        setEditable( false );
+        drawingPanel.removeText(this);
+    }
+
+    public void focusGained(FocusEvent e) {}
+
+//  Implement MouseListener
+
+    public void mouseClicked( MouseEvent e )
+    {
+        if (e.getClickCount() == 2)
+            setEditable( true );
+    }
+
+    public void mouseEntered( MouseEvent e ) {}
+
+    public void mouseExited( MouseEvent e ) {}
+
+    public void mousePressed( MouseEvent e ) {}
+
+    public void mouseReleased( MouseEvent e ) {}
+
+//  Implement DocumentListener
+
+    public void insertUpdate(DocumentEvent e)
+    {
+        updateSize();
+    }
+
+    public void removeUpdate(DocumentEvent e)
+    {
+        updateSize();
+    }
+
+    public void changedUpdate(DocumentEvent e) {}
+
+    private void updateSize()
+    {
+        setSize( getPreferredSize() );
+    }
+}
